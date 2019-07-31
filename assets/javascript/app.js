399d02c939c6614b984085eb1e91f3b2fdf13afa
llamadu <llamadu@users.noreply.github.com>
2019-07-31 11:59:46 -0700

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){ 
+ // Initialize Firebase
+
+
+	// Referencing the firebase
+	var database= firebase.database();
+
+	//Initial train values
+	var train = {
+		Name: "",
+		Destination: "",
+		Time: "",
+		Frequency: 0,
+	};
+		//Button for adding trains
+
+
+		//Get new train input
+	
+		//Create temporary object for holding train new
+
+	
+		//Uploads train data to the database
+
+		
+		// Clear all of the text-boxes
+	
+
+		//Prevents new page
+	
+
+	//Update every minute
+
+	//Create Firebase event for adding trains to the database and a row in the html with entries
+	
+		//Add stored trains to the schedule
+			//assigning train names to a variable
+		
+
+			//Destination
+			
+				//Add a new line for the next train
+				
+
+			//Frequency
+		
+				//Add a new line for the next train
+				
+			//Calculate arrival times and minutes away on page load.
+			//Get the first train time stored in the database.
+			
+
+						
+
+				//function to calculate time
+				//Current Time
+			
+				//Difference between the times
+				
+
+				//Time apart 
+				
+				//Minutes until train
+				
+
+				//Next Train
+				
+	 	
+				//Next train arrival
+				
+				//Add a new line for the next train
+				
+
+				//Minutes until next train
+				
+				//Add a new line for the next train
+				
+				// Log errors to console
+		
+			
+	
+});	
